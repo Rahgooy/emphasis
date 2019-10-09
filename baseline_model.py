631ab4cde0d496bc2e1e4ac2ff4ed9aa96845747
Mostafa Rahgouy <mostafarahgouy@gmail.com>
2019-10-09 23:23:46 +0330

    updated

--- baseline_model.py
+++ baseline_model.py
@@ -1,6 +1,5 @@
 import Read_data_and_Write_results as rw
-from collections import defaultdict
-from nltk.stem import PorterStemmer as stemmer
+from word_frequency_model import WordFrequencyModel as wfm
 import argparse
 import os
 
@@ -19,25 +18,6 @@ def mkdir(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
-def build_model(bio_freqs_word, occurrences):
-    model = {}
-    for word ,bio_freq in bio_freqs_word.items():
-            model[word] = (bio_freq / ( occurrences[word] * 9))
-    return model
-
-def predict(word_lsts, model):
-    predictions = []
-    for i in range(len(word_lsts)):
-        word_prediction = []
-        for j in range(len(word_lsts[i])):
-            word_stem = stemmer().stem(word_lsts[i][j].lower())
-            if word_stem in model.keys():
-                word_prediction.append(model[word_stem])
-            else:
-                word_prediction.append(0)
-        predictions.append(word_prediction)
-    return predictions
-
 
 if __name__ == "__main__":
     args = get_args()
@@ -49,22 +29,13 @@ if __name__ == "__main__":
     path_to_output = os.path.join(path_to_output , "res")
     mkdir(path_to_output)
 
-    _, word_lsts, _, bio_freqs, _, _  = rw.read_data(path_to_train)
+    _, word_lsts, _, bio_lsts, _, _  = rw.read_data(path_to_train)
 
-    print("training the model ... ")
-    bio_freqs_word = defaultdict(int)
-    occurrences_word = defaultdict(int)
-    for i in range(len(bio_freqs)):
-        for j in range(len(bio_freqs[i])):
-            bio = bio_freqs[i][j].split("|")
-            word_stem = stemmer().stem(word_lsts[i][j].lower())
-            bio_freqs_word[word_stem] += (int(bio[0]) + int(bio[1]))
-            occurrences_word[word_stem] += 1
+    print("training the model ... ")    
+    wfm_model = wfm(word_lsts, bio_lsts)
+    wfm_model.fit()
     
-    
-    model = build_model(bio_freqs_word, occurrences_word)
-
     print("testing the model ... ")
     word_id_lsts, word_lsts, _, _, _, _  = rw.read_data(path_to_test)
-    predictions = predict(word_lsts, model)
+    predictions = wfm_model.predict(word_lsts)
     rw.write_results(word_id_lsts, word_lsts, predictions, os.path.join(path_to_output, "submission.txt"))
