6fa9cc73e2adb5e6bd28330076cbe9ed322762d4
Mostafa Rahgouy <mostafarahgouy@gmail.com>
2019-11-12 01:43:20 +0330

    build_vocab & word_conditional scripts updated

--- build_vocabs.py
+++ build_vocabs.py
@@ -1,7 +1,8 @@
 import numpy as np
 import os
 import Read_data_and_Write_results as rw
-
+from nltk.stem import PorterStemmer as stemmer
+import codecs
 
 def build_vocab(str_lsts):
     vocab_encoder = {}
@@ -17,7 +18,7 @@ def create_vocabs(input_path, output_path):
     _, word_vocab, _, _, _, pos_vocab = rw.read_data(input_path)
     
     pos_vocab = [item for innerlist in pos_vocab for item in innerlist]
-    word_vocab = [item for innerlist in word_vocab for item in innerlist]
+    word_vocab = [stemmer().stem(item.lower()) for innerlist in word_vocab for item in innerlist]
     
     pos_encoder, pos_decoder = build_vocab(pos_vocab)
     word_encoder, word_decoder = build_vocab(word_vocab)
@@ -29,3 +30,36 @@ def create_vocabs(input_path, output_path):
     np.save(os.path.join(output_path, "word_decoder.npy"), word_decoder)
     np.save(os.path.join(output_path, "word_encoder.npy"), word_encoder)
     
+def create_wordvocab(word_lsts, words_id, out_path):
+    words = [stemmer().stem(item.lower()) for innerlist in word_lsts for item in innerlist]
+    ids = [item for innerlist in words_id for item in innerlist]
+    word_vocab = []
+    with codecs.open(out_path, "w", encoding = "utf-8") as f:
+            
+        for i in range(len(word_lsts)):
+            for j in range(len(word_lsts[i])):
+                if word_lsts[i][j] not in word_vocab:
+                    word_vocab.append(stemmer().stem(word_lsts[i][j].lower()))
+                    f.write(words_id[i][j] + "\t" + word_lsts[i][j] + "\n")
+    return word_vocab
+
+
+def get_id_of_none_wordvocab(model_vocab_path, test_vocab_path, output_path):
+    
+    id_of_none_wordvocab = []
+    
+    with codecs.open(model_vocab_path, "r", encoding = "utf-8") as f:
+        model = f.read()
+    model_words_id = [item.split("\t")[0] for item in model.split("\n") if len(item) != 0]
+    model_words = [item.split("\t")[1] for item in model.split("\n") if len(item) != 0]
+    
+    with codecs.open(test_vocab_path, "r", encoding = "utf-8") as f:
+        test = f.read()
+    test_words_id = [item.split("\t")[0] for item in test.split("\n") if len(item) != 0]
+    test_words = [item.split("\t")[1] for item in test.split("\n") if len(item) != 0]
+    
+                
+    with codecs.open(output_path, "w", encoding = "utf-8") as f:
+        for i in range(len(test_words)):
+            if test_words[i] not in model_words:
+                f.write(test_words_id[i] + "\t" + test_words[i] + "\n")
--- word_conditional_model.py
+++ word_conditional_model.py
@@ -5,6 +5,7 @@
 import numpy as np
 from nltk.stem import PorterStemmer as stemmer
 import pickle 
+import build_vocabs as bv
 
 class WordConditionalModel:
 
@@ -46,14 +47,10 @@ class WordConditionalModel:
         self.__model[w_i][w_j] = (bold_wi / occurrenece_of_wi_and_wj) if occurrenece_of_wi_and_wj else 0
         #print("[{}], [{}] = {}".format(self.__words[w_i], self.__words[w_j], self.__model[w_i][w_j]))
 
-    def fit(self, words_lsts, bios_lsts):
+    def fit(self, words_lsts, words_id, bios_lsts, path = "Expreiment.txt"):
         print("training WordConditionalModel....")
-        self.__words = []
-        words_vocab = [stemmer().stem(word.lower()) for innerlist in words_lsts for word in innerlist]
-        for word in words_vocab:
-            if word not in self.__words:
-                self.__words.append(word)
-        
+        self.__words = bv.create_wordvocab(words_lsts, words_id, path)
+                
         self.__model = self.__build_matrix(len(self.__words), len(self.__words))
         self.__words_matrix = self.__represent_sentences_as_matrix(words_lsts)
         self.__bios_matrix = self.__represent_bios_as_matrix(bios_lsts, words_lsts)
@@ -65,9 +62,7 @@ class WordConditionalModel:
                 self.__calculate_condetional_probs(i, j)
         print()
 
-    def predict(self, word_lsts, words_id):
-        self.__get_id_of_none_wordvocab(word_lsts, words_id)
-        print(self.__none_words_vocab)
+    def predict(self, word_lsts):
         predictions = []
         for i in range(len(word_lsts)):
             prediction = []
@@ -87,20 +82,11 @@ class WordConditionalModel:
             predictions.append(prediction)
         return predictions
     
-    def __get_id_of_none_wordvocab(word_lsts, words_id):
-        self.__none_words_vocab = []
-        for i in range(len(word_lsts)):
-            for j in range(len(word_lsts[i])):
-                word_ij = stemmer().stem(word_lsts[i][j].lower())
-                if word_ij not in self.__words:
-                    self.__none_words_vocab.append(words_id[i][j])
-        self.__none_words_vocab = none_words_vocab
-        
     
     def save(self, path):
-        model = {"__words":self.__words, "__model":self.__model, "__none_words":self.__none_words_vocab}
+        model = {"__words":self.__words, "__model":self.__model}
         pickle.dump(model,open(path,"wb"))
 
     def load(self, path):
         model = pickle.load(open(path, "rb"))
-        self.__words, self.__model, self.__none_words_vocab = model["__words"], model["__model"], model["__none_words"]
+        self.__words, self.__model = model["__words"], model["__model"]
