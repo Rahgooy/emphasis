d92aae239d2296296325a3b0c0663c1135fa5c31
Mostafa Rahgouy <mostafarahgouy@gmail.com>
2019-11-13 03:17:27 +0330

    word_conditional_model.py updated

--- word_conditional_model.py
+++ word_conditional_model.py
@@ -5,61 +5,26 @@
 import numpy as np
 from nltk.stem import PorterStemmer as stemmer
 import pickle 
-import build_vocabs as bv
 
 class WordConditionalModel:
 
     def __init__(self):
-        self.__words_matrix = 0
-        self.__bios_matrix = 0
-        self.__model = 0
-        self.__words = []
-        self.__none_words_vocab = []
-    
-    def __build_matrix(self, number_of_sentences, number_of_words):
-        word_matrix = np.zeros((number_of_sentences, number_of_words))
-        return word_matrix
+           self.__model = 0
 
-
-    def __represent_sentences_as_matrix(self, words_lsts):
-        matrix = self.__build_matrix(len(words_lsts * 9), len(self.__words))
-        for i in range(len(words_lsts)):
-            for j in range(len(words_lsts[i])):
-                mj = self.__words.index( stemmer().stem(words_lsts[i][j].lower()))
-                matrix[i*9:(i+1)*9, mj] = 1
-        return matrix
-
-    def __represent_bios_as_matrix(self, bios_lsts, words_lsts):
-        matrix = self.__build_matrix(len(words_lsts * 9), len(self.__words))
-        for i in range(len(words_lsts)):
-            for j in range(len(words_lsts[i])):
-                d = self.__words.index( stemmer().stem(words_lsts[i][j].lower()))
-                for m in range(9):
-                    if bios_lsts[i][j][m * 2] == "I" or bios_lsts[i][j][m * 2] == "B":
-                        matrix[(i * 9) + m][d] = 1
-        return matrix
-
-    def __calculate_condetional_probs(self, w_i, w_j):
+    def __calculate_condetional_probs(self,x, y, w_i, w_j):
         occurrenece_of_wi_and_wj, bold_wi = 0, 0
-        occurrenece_of_wi_and_wj = (self.__words_matrix[:, w_i] * self.__words_matrix[:, w_j]).sum()
-        y = self.__words_matrix[:, w_i] * self.__words_matrix[:, w_j]
-        bold_wi = (y * self.__bios_matrix[:, w_i]).sum()
+        occurrenece_vector = x[:, w_i] * x[:, w_j]
+        occurrenece_of_wi_and_wj = occurrenece_vector.sum()
+        bold_wi = (occurrenece_vector * y[:, w_i]).sum()
         self.__model[w_i][w_j] = (bold_wi / occurrenece_of_wi_and_wj) if occurrenece_of_wi_and_wj else 0
-        #print("[{}], [{}] = {}".format(self.__words[w_i], self.__words[w_j], self.__model[w_i][w_j]))
 
-    def fit(self, words_lsts, words_id, bios_lsts, path = "Expreiment.txt"):
+    def fit(self, x, y):
         print("training WordConditionalModel....")
-        self.__words = bv.create_wordvocab(words_lsts, words_id, path)
-                
-        self.__model = self.__build_matrix(len(self.__words), len(self.__words))
-        self.__words_matrix = self.__represent_sentences_as_matrix(words_lsts)
-        self.__bios_matrix = self.__represent_bios_as_matrix(bios_lsts, words_lsts)
-        print("vocab lenghts: " , len(self.__words))
-        for i in range(len(self.__words)):
+        for i in range(x.shape[1]):
             if i%100 == 0:
                 print(i , end = ' ')
-            for j in range(len(self.__words)):
-                self.__calculate_condetional_probs(i, j)
+            for j in range(x.shape[1]):
+                self.__calculate_condetional_probs(x, y, i, j)
         print()
 
     def predict(self, word_lsts):
