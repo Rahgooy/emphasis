33bcb9e15d793bb021602ed19452a717ba3cc2f2
Mostafa Rahgouy <mostafarahgouy@gmail.com>
2019-11-14 05:35:21 +0330

    data_helpers script updated

--- data_helpers.py
+++ data_helpers.py
@@ -3,35 +3,48 @@ import Read_data_and_Write_results as rw
 from nltk.stem import PorterStemmer as stemmer
 import vocabs as v
     
-def reader(path):
+def read(path, word2Id = None):
         
-    def represent_x_as_matrix(words_lsts, word2Id):
-        matrix = build_matrix(len(words_lsts * 9), len(word2Id))
-        for i in range(len(words_lsts)):
-            for j in range(len(words_lsts[i])):
-                wordId = word2Id[stemmer().stem(words_lsts[i][j].lower())]
-                matrix[i*9:(i+1)*9, wordId] = 1
+    def get_one_hot_matrix(mtx, word2Id, lenght = 9):
+        matrix = build_matrix(len(mtx.shape[0] * lenght), len(word2Id))
+        for i in range(len(mtx)):
+            temp = [Id for Id in mtx[i] if Id != -1]
+            matrix[i*lenght:(i+1)*lenght, temp] = 1
         return matrix
-
-
-    def represent_y_as_matrix(bios_lsts, words_lsts, word2Id):
-        matrix = build_matrix(len(words_lsts * 9), len(word2Id))
-        for i in range(len(words_lsts)):
-            for j in range(len(words_lsts[i])):
-                wordId = word2Id[stemmer().stem(words_lsts[i][j].lower())]
-                for m in range(9):
-                    if bios_lsts[i][j][m * 2] == "I" or bios_lsts[i][j][m * 2] == "B":
-                        matrix[(i * 9) + m][wordId] = 1
-        return matrix
-
 
     def build_matrix(number_of_sentences, number_of_words):
         word_matrix = np.zeros((number_of_sentences, number_of_words))
-        return word_matrix
-    
+        return word_matrix          
+
+    def represent_x_as_matrix(word_lsts, word2Id):
+        x, temp = [], []
+        for i in range(len(word_lsts)):
+            temp = [word2Id[stemmer().stem(word.lower())]
+                    if stemmer().stem(word.lower()) in word2Id else -1 
+                    for word in word_lsts[i]]
+            x.append(temp)
+        return x
+
+    def represent_y_as_matrix(bios_lsts, words_lsts, word2Id):
+        y = []
+        for i in range(len(words_lsts)):
+            for m in range(9):
+                temp = []
+                for j in range(len(words_lsts[i])):
+                    wordId = word2Id[stemmer().stem(words_lsts[i][j].lower())] 
+                    if bios_lsts[i][j][m * 2] == "I" or bios_lsts[i][j][m * 2] == "B":
+                        temp.append(wordId)
+                y.append(temp)
+        return y   
+
     words_id, word_lsts, bio_lsts, freq_lsts, prob_lsts, pos_lsts = rw.read_data(path)
     Id2word, word2Id = v.build_vocab(word_lsts)
     dataId2word = v.get_dataId2word(word_lsts, words_id)
     x = represent_x_as_matrix(word_lsts, word2Id)
     y = represent_y_as_matrix(bio_lsts, word_lsts, word2Id)
+    x_one_hot = get_one_hot_matrix(x, word2Id)
+    y_one_hot = get_one_hot_matrix(y, word2Id, 1)
     return x, y, word2Id, Id2word, dataId2word
+
+
+
