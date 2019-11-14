3126997f9cf501f33ac2c4be76ffd182f8d571db
Mostafa Rahgouy <mostafarahgouy@gmail.com>
2019-11-14 06:12:38 +0330

    data_helpers scripts updated

--- data_helpers.py
+++ data_helpers.py
@@ -5,36 +5,32 @@ import vocabs as v
     
 def read(path, word2Id = None):
         
-    def get_one_hot_matrix(mtx, word2Id, lenght = 9):
-        matrix = build_matrix(len(mtx.shape[0] * lenght), len(word2Id))
+    def get_one_hot_matrix(mtx, n):
+        matrix = np.zeros([mtx.shape[0], n])
         for i in range(len(mtx)):
             temp = [Id for Id in mtx[i] if Id != -1]
-            matrix[i*lenght:(i+1)*lenght, temp] = 1
+            matrix[i, temp] = 1
         return matrix
 
-    def build_matrix(number_of_sentences, number_of_words):
-        word_matrix = np.zeros((number_of_sentences, number_of_words))
-        return word_matrix          
-
     def represent_x_as_matrix(word_lsts, word2Id):
-        x, temp = [], []
+        x, row = [], []
         for i in range(len(word_lsts)):
-            temp = [word2Id[stemmer().stem(word.lower())]
+            row = [word2Id[stemmer().stem(word.lower())]
                     if stemmer().stem(word.lower()) in word2Id else -1 
                     for word in word_lsts[i]]
-            x.append(temp)
+            x.append(row * 9)
         return x
 
     def represent_y_as_matrix(bios_lsts, words_lsts, word2Id):
         y = []
         for i in range(len(words_lsts)):
             for m in range(9):
-                temp = []
+                row = []
                 for j in range(len(words_lsts[i])):
                     wordId = word2Id[stemmer().stem(words_lsts[i][j].lower())] 
                     if bios_lsts[i][j][m * 2] == "I" or bios_lsts[i][j][m * 2] == "B":
-                        temp.append(wordId)
-                y.append(temp)
+                        row.append(wordId)
+                y.append(row)
         return y   
 
     words_id, word_lsts, bio_lsts, freq_lsts, prob_lsts, pos_lsts = rw.read_data(path)
@@ -43,7 +39,7 @@ def read(path, word2Id = None):
     x = represent_x_as_matrix(word_lsts, word2Id)
     y = represent_y_as_matrix(bio_lsts, word_lsts, word2Id)
     x_one_hot = get_one_hot_matrix(x, word2Id)
-    y_one_hot = get_one_hot_matrix(y, word2Id, 1)
+    y_one_hot = get_one_hot_matrix(y, word2Id)
     return x, y, word2Id, Id2word, dataId2word
 
 
